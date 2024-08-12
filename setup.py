db8244738d754de010e0da3b844b12b946f84086
DrKaiyangZheng <131530767+DrKaiyangZheng@users.noreply.github.com>
2024-08-12 23:20:19 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,17 +22,17 @@ setup(
     python_requires='>=3.9',
     # https://packaging.python.org/discussions/install-requires-vs-requirements/
     install_requires=[
-        'numpy>=1.25',
-        'pandas=1.5',
-        'entrez-direct =16.2'
+        'numpy >=1.25',
+        'pandas >=1.5',
+        'entrez-direct >=16.2'
         'seqkit >=2.5'
         'prodigal >=2.6'
         'setuptools>=65.6.3',
-        'scipy>=1.10.1',
-        'networkx>=3.1',
-        'tqdm>=4.65.0',
-        'biopython>=1.78',
-        'diamond>=0.9',
+        'scipy >=1.10.1',
+        'networkx >=3.1',
+        'tqdm >=4.65.0',
+        'biopython >=1.78',
+        'diamond >=0.9',
     ],
     package_data={
         'vitap': [
