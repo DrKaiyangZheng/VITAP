52b1412b3b4c3036e63038d4e1b3845f995a9252
DrKaiyangZheng <131530767+DrKaiyangZheng@users.noreply.github.com>
2026-04-16 18:33:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (curr_dir / "README.md").read_text()
 # https://github.com/pypa/sampleproject/blob/master/setup.py
 setup(
     name='VITAP',
-    version='1.11',
+    version='1.12',
     packages=find_packages(exclude=['images']),
     description='Viral Taxonomic Assignment Pipeline',
     long_description=long_description,
